f1ef36a45e3809fc337fa85ee6f43db383f80572
Masterchief-07 <kjonathan1999@hotmail.com>
2023-05-06 15:50:45 +0000

    testing new plot

--- main.cpp
+++ main.cpp
@@ -5,14 +5,20 @@
 
 int main() {
     Plot myploter{"../venv/bin/python"};
-    // std::vector<double> x = {0.0, 1.0, 2.0, 3.0, 4.0};
-    // std::vector<double> y = {0.0, 2.0, 4.0, 6.0, 8.0};
-    int n = 5000; // number of data points
-    std::vector<double> x(n),y(n);
+    int n = 50; // number of data points
+    std::vector<double> x(n),y(n), x_2(n), y_2(n);
     for(int i=0; i<n; ++i) {
         double t = 2*M_PI*i/n;
         x.at(i) = 16*sin(t)*sin(t)*sin(t);
         y.at(i) = 13*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t);
     }
-    myploter(x, y);
+    for(int i=0; i<n; ++i) {
+        double t = 2*M_PI*i/n;
+        x_2.at(i) = 10*sin(t)*sin(t)*sin(t);
+        y_2.at(i) = 7*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t);
+    }
+    // myploter(x, y);
+    myploter.plot(x, y);
+    myploter.plot(x_2, y_2, "b*");
+    myploter.show();
 }
