2a5e0627cb57d967d03126ce4807bbb057479ebc
Masterchief-07 <kjonathan1999@hotmail.com>
2023-05-07 00:27:08 +0000

    started testing vector

--- main.cpp
+++ main.cpp
@@ -2,35 +2,18 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <vector.hpp>
 
 int main() {
-    Plot myploter{"../venv/bin/python"};
-    int n = 10000; // number of data points
-    std::vector<double> x(n),y(n), x_2(n), y_2(n),  x_3(n), y_3(n),  x_4(n), y_4(n);
-    for(int i=0; i<n; ++i) {
-        double t = 2*M_PI*i/n;
-        x.at(i) = 16*sin(t)*sin(t)*sin(t);
-        y.at(i) = 13*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t);
-    }
-    for(int i=0; i<n; ++i) {
-        double t = 2*M_PI*i/n;
-        x_2.at(i) = 10*sin(t)*sin(t)*sin(t);
-        y_2.at(i) = 7*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t);
-    }
-    for(int i=0; i<n; ++i) {
-        double t = 2*M_PI*i/n;
-        x_3.at(i) = 20*sin(t)*sin(t)*sin(t);
-        y_3.at(i) = 17*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t);
-    }
-    for(int i=0; i<n; ++i) {
-        double t = 2*M_PI*i/n;
-        x_4.at(i) = 6*sin(t)*sin(t)*sin(t);
-        y_4.at(i) = 2*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t);
-    }
-    // myploter(x, y);
-    myploter.plot(x, y);
-    myploter.plot(x_2, y_2, "b*");
-    myploter.plot(x_3, y_3, "g*");
-    myploter.plot(x_4, y_4, "y*");
-    myploter.show();
+    krain::Vector<float, 1, 3> direction(3);
+    direction.get(0,0) = 3;
+    direction.get(0,1) = 6;
+    direction.get(0,2) = 9;
+    std::cout<<direction.get(0, 1)<<"\n";
+    auto newdirection = direction + 6;
+    newdirection -= 3;
+    std::cout<<krain::Vector<float, 1, 3>::sum(direction)<<"\n";
+    std::cout<<newdirection.sum()<<"\n";
+
+    return 0;
 }
