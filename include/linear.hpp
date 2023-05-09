57eb604151593ebab2ae03b8be195644b35bf275
Masterchief-07 <kjonathan1999@hotmail.com>
2023-05-09 03:23:18 +0000

    started implementation

--- include/linear.hpp
+++ include/linear.hpp
@@ -1,21 +1,32 @@
 #pragma once
-#include <vector>
-#include <tensor.hpp>
+// #include <vector>
+#include "vector.hpp"
 
 namespace krain
 {
+template<SCALAR_C scalar>
 class LinearRegression{
     public:
-    LinearRegression();
+    LinearRegression(){};
     ~LinearRegression(){}
-    void train(std::vector<float>& input, std::vector<float>& target);
-    std::vector<float> predict(std::vector<float>& input);
+    template<size_t columns>
+    void train(Vector<scalar, 1, columns>& input, Vector<scalar, 1, columns>& target)
+    {
+        auto output = input * w + b;
+
+    }
+    template<size_t columns>
+    Vector<scalar, 1, columns> predict(Vector<scalar, 1, columns>& input)
+    {
+
+    }
 
     private:
     float wGrad(std::vector<float>& input);
     float bGrad();
 
     private:
-    float w{0}, b{0}, w_grad{0}, b_grad{0};
+    scalar w{0}, b{0}, w_grad{0}, b_grad{0};
+
 };
 }
