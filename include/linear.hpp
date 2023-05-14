a5bf5b5970ec2d4f3bb9e78fcaae2232dd2a7c79
Masterchief-07 <kjonathan1999@hotmail.com>
2023-05-14 07:50:59 +0000

    implemented linear without testing

--- include/linear.hpp
+++ include/linear.hpp
@@ -4,29 +4,80 @@
 
 namespace krain
 {
-template<SCALAR_C scalar>
+template<typename scalar>
+requires SCALAR<scalar>
 class LinearRegression{
     public:
-    LinearRegression(){};
+    LinearRegression(){}
     ~LinearRegression(){}
     template<size_t columns>
-    void train(Vector<scalar, 1, columns>& input, Vector<scalar, 1, columns>& target)
+    void train(const Vector<scalar, 1, columns>& input, const Vector<scalar, 1, columns>& target)
     {
-        auto output = input * w + b;
+        scalar loss = 1;
+        while(loss>0.001)
+        {
+            loss = steps(input, target);
+        }
+        // w_grad = 
+    }
+    template<size_t columns>
+    Vector<scalar, 1, columns> predict(const Vector<scalar, 1, columns>& input)
+    {
+        return b + w * input;
+    }
+    void setLR(double lr=0.001)
+    {
+        m_lr = lr;
+    }
+    scalar getW() const
+    {
+        return w;
+    }
+
+    scalar getB() const
+    {
+        return b;
+    }
+    scalar getWGrad() const
+    {
+        return w_grad;
+    }
+    scalar getBGrad() const
+    {
+        return b_grad;
+    }
+
+    private:
+    template<size_t columns>
+    scalar steps(const Vector<scalar, 1, columns>& input, const Vector<scalar, 1, columns>& target)
+    {
+        auto output = b + w * input ;
+        std::cout<<"output: "<<output<<"\n";
+        auto error = output - target;
+        std::cout<<"error: "<<error<<"\n";
+        auto loss = (error^2).sum();
+        std::cout<<"loss: "<<loss<<"\n";
+        w_grad = wGrad(input, error);
+        b_grad = bGrad(error);
+        w -= m_lr * w_grad;
+        b -= m_lr * b_grad;
+
+        return loss;
+    }
+    template<size_t columns>
+    scalar wGrad(const Vector<scalar, 1, columns>& input, const Vector<scalar, 1, columns>& error)
+    {
+        return 2 * (input * error).sum() / (scalar) input.size();
 
     }
     template<size_t columns>
-    Vector<scalar, 1, columns> predict(Vector<scalar, 1, columns>& input)
+    scalar bGrad(const Vector<scalar, 1, columns>& error)
     {
-
+        return 2 * (error).sum() / (scalar) error.size();
     }
 
     private:
-    float wGrad(std::vector<float>& input);
-    float bGrad();
-
-    private:
-    scalar w{0}, b{0}, w_grad{0}, b_grad{0};
+    scalar w{0}, b{0}, w_grad{0}, b_grad{0}, m_lr{0.001};
 
 };
 }
