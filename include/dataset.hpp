69c2016e1085f4236b370a9317ad81926fe465de
Masterchief-07 <kjonathan1999@hotmail.com>
2023-06-04 19:10:46 +0000

    started implementing csv file reader

---
+++ include/dataset.hpp
@@ -0,0 +1,27 @@
+//reading and using dataset
+#pragma once
+
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <cassert>
+
+class CSV
+{
+    public:
+    CSV(std::filesystem::path const& filepath);
+
+    private:
+    std::ifstream m_file;
+};
+
+CSV::CSV(std::filesystem::path const& filepath):m_file{filepath}
+{
+    assert(m_file.is_open());
+
+}
+
+class Datasets
+{
+
+};
