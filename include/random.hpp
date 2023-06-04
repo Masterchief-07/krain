fb88de1dee155f9714241822dbe2d72b9fe5b7db
Masterchief-07 <kjonathan1999@hotmail.com>
2023-06-04 19:10:30 +0000

    started implementing random generator

--- include/random.hpp
+++ include/random.hpp
@@ -1,8 +1,48 @@
 #pragma once
+#include <vector.hpp>
+#include <concepts>
+#include <random>
 
 //------generating random values-----------------------
-
-class Random
+namespace krain::random
 {
+    template<typename T>
+    concept FLOATING = std::is_floating_point_v<T>;
+    //probability mass functions
+    template<typename T>
+    requires FLOATING<T>
+    class Uniform
+    {
+        public:
+        Uniform(const uint seed):m_seed{seed}, m_urgb{seed};
+        {
+
+        }
+        T gen(T min, T max)
+        {
+
+        }
+        void seed(uint seed)
+        {
+            m_seed = seed;
+        }
+        int seed() const
+        {
+            return m_seed;
+        }
+        private:
+        uint m_seed;
+        std::mt19937 m_urgb;
+
+
+    };
+
+    //probability discret functions
+    template<typename T>
+    requires FLOATING<T>
+    class PDF
+    {
+
+    };
 
 };
