8b8e92bb616dd15edbff6ba38b561f7c51d4d783
Masterchief-07 <kjonathan1999@hotmail.com>
2023-06-30 02:34:04 +0000

    seperate csv and dataset into differents elements

--- include/dataset/dataset.hpp
+++ include/dataset/dataset.hpp
@@ -2,24 +2,7 @@
 #pragma once
 
 #include <string>
-#include <filesystem>
-#include <fstream>
-#include <cassert>
-
-class CSV
-{
-    public:
-    CSV(std::filesystem::path const& filepath);
-
-    private:
-    std::ifstream m_file;
-};
-
-CSV::CSV(std::filesystem::path const& filepath):m_file{filepath}
-{
-    assert(m_file.is_open());
-
-}
+#pragma once
 
 class Datasets
 {
