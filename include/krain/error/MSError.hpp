8144aea27872ff3a3da72b7a0338622b60356f74
Masterchief-07 <kjonathan1999@hotmail.com>
2023-06-30 02:59:56 +0000

    reconfigure error in a folder

--- include/krain/error/MSError.hpp
+++ include/krain/error/MSError.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-
+namespace krain
+{
 class MSError{
 
     public:
@@ -10,8 +11,8 @@ class MSError{
     double operator() (T y_hat, T y)
     {
         auto result = y_hat - y
-
         return 0;
     }
 
 };
+}
